1a2463e31824db1e99aef127f4d0f0eaa5900a49
Lewis Cummings <cummingsjr00@gmail.com>
2024-08-06 11:20:08 +0000

    Update files

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path import os
+from pathlib import Path
+import os
 import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q6m_@bd^e+!&1h(51hursclg82c512et13=ui5w#pz8(26)*gf'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
