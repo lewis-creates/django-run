40b3f7a94d9c31b774dcbc76a73e9947cead123a
Lewis Cummings <cummingsjr00@gmail.com>
2024-08-15 11:57:54 +0000

    Set up for django

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lewiscreates-djangorun-wfsnf06xd97.ws.codeinstitute-ide.net']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-lewiscreates-djangorun-wfsnf06xd97.ws.codeinstitute-ide.net',
+]
+
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
